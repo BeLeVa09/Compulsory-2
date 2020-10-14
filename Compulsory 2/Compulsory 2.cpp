982d91d8f65d8be1e4d7eb7181768d9c7460a38f
Bernt Leander Vargan <b.leandervargan@gmail.com>
2020-10-14 17:33:03 +0200

    Done!

--- Compulsory 2/Compulsory 2.cpp
+++ Compulsory 2/Compulsory 2.cpp
@@ -4,19 +4,24 @@
 #include <sstream>
 #include <ctime>
 #include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
 // Initializing functions
 void createDeck();
 void game();
-void drawCard(int offset, int chosenCard);
+void drawCard(int offset, int chosenCard, int player, bool bank);
 
 // Setting ascii values for card suits
 const char hearts = 3;
 const char diamonds = 4;
 const char clubs = 5;
 const char spades = 6;
+
+// I know global variables are evil, but this is the only way I got the money system to work properly
+int playerMoney{ 100 };
+int houseMoney{ 100 };
 
 // Creating an object for every unique card
 struct Card {
@@ -30,6 +35,7 @@ struct Card {
 
 vector <Card> deck(52);
 vector <vector <char>> cardPrint(6, vector <char>(200));
+vector <vector <char>> aiCardPrint(6, vector <char>(200));
 
 int main()
 {
@@ -41,6 +47,7 @@ int main()
         for (int i{ 0 }; i < 6; ++i) {
             for (int j{ 0 }; j < 100; ++j) {
                 cardPrint[i][j] = ' ';
+                aiCardPrint[i][j] = ' ';
             }
         }
         createDeck();
@@ -54,8 +61,7 @@ int main()
         cout << "\t\t\t\\____/|_|\\__,_|\\___|_|\\_\\____/\\___,_|\\___|_|\\_\\\n\n";
 
         cout << "\t\t\t\t\t    1. Play\n";
-        cout << "\t\t\t\t\t    2. Rules\n";
-        cout << "\t\t\t\t\t    3. Exit\n";
+        cout << "\t\t\t\t\t    2. Exit\n";
 
         char input = _getch();
 
@@ -65,11 +71,7 @@ int main()
             break;
 
         case '2':
-            break;
-
-        case '3':
             quit = true;
-    
             break;
 
         default:
@@ -80,63 +82,139 @@ int main()
 }
 
 void game() {
-    system("cls");
 
-    int playerMoney{ 100 };
-    int houseMoney{ 100 };
     bool hold{ false };
     int chosenCard{ 0 };
     int cardsDrawn{ 0 };
+    int playerPoints{ 0 };
+    int housePoints{ 0 };
+    int bet{ 0 };
+    int pot{ 0 };
 
     do {
         system("cls");
+        cout << "Pay in starts at 10$\n";
+        cout << "One cannot bet more than another player has\n";
+        cout << "Your money: " << playerMoney << "$\n";
+        cout << "House money: " << houseMoney << "$\n\n";
+
+        cout << "Bet: ";
+        cin >> bet;
+    } while (bet > houseMoney && bet > playerMoney);
+
+    playerMoney -= bet;
+    houseMoney -= bet;
+
+    pot = 2 * bet;
+
+    system("cls");
+
+    cout << ".--------.\n";
+    cout << "|   __   |\n";
+    cout << "|  |  |  |\n";
+    cout << "|    /   |\n";
+    cout << "|    .   |\n";
+    cout << "'--------'\n";
+
+    do { // ----------GAME LOOP----------
         bool validInput{ false };
 
-        cout << ".--------.\n";
-        cout << "|   __   |\n";
-        cout << "|  |  |  |\n";
-        cout << "|    /   |\n";
-        cout << "|    .   |\n";
-        cout << "|________|\n";
-        cout << "'--------'\n\n";
-
-        cout << "(D) to draw a card\n";
-        cout << "(H) to hold your hand\n";
-
         if (cardsDrawn != 0) {
-            drawCard(cardsDrawn, chosenCard);
+            drawCard(cardsDrawn, chosenCard, 1, false);
+        }
+        cout << "Your Points: " << playerPoints;
+
+        if (playerPoints > 21) {
+            cout << "\n\nHouse wins!\n";
+            cout << "You lost " << bet << "$\n";
+            houseMoney += bet;
+            _getch();
+            return;
+        }
+        else {
+            cout << "\n\n";
+            cout << "The pot is: " << pot << "$\n";
+            cout << "(D) to draw a card\n";
+            cout << "(H) to hold your hand\n\n";
+            cout << "House Points: " << housePoints << "\n";
+            cout << ".--------.\n";
+            cout << "|   __   |\n";
+            cout << "|  |  |  |\n";
+            cout << "|    /   |\n";
+            cout << "|    .   |\n";
+            cout << "'--------'\n";
         }
 
         do {
             char input = _getch();
             switch (toupper(input)) {
             case 'D':
-                ++cardsDrawn;
                 validInput = true;
                 do {
                     chosenCard = rand() % 52;
                 } while (deck[chosenCard].drawn == true);
+                if (deck[chosenCard].isAce == true) {
+                    cout << "Do you want the Ace to be of value 11 instead of 1?\n";
+                    cout << "1. Yes\n";
+                    cout << "2. No\n";
+                    char input = _getch();
+                    if (input == '1') {
+                        playerPoints += 10;
+                    }
+                }
+                playerPoints += deck[chosenCard].value;
+                ++cardsDrawn;
                 break;
 
-            case 'H':
+            case 'H': // AI start---------------------------------------------------------------------------------------------------------------------------------
                 hold = true;
                 validInput = true;
-                break;
+                cardsDrawn = 0;
+
+                do {
+                    system("cls");
+                    do {
+                        chosenCard = rand() % 52;
+                    } while (deck[chosenCard].drawn == true);
+                    housePoints += deck[chosenCard].value;
+                    ++cardsDrawn;
+
+                    drawCard(cardsDrawn, chosenCard, 1, true);
+                    cout << "Your Points: " << playerPoints << "\n\n";
+                    cout << "The pot is: " << pot << "$\n";
+                    cout << "(D) to draw a card\n";
+                    cout << "(H) to hold your hand\n\n";
+                    cout << "House Points: " << housePoints << "\n";
+                    drawCard(cardsDrawn, chosenCard, 0, true);
+
+                    Sleep(1250);
+                } while (housePoints <= playerPoints);
+
+                if (housePoints > 21) {
+                    cout << "\nPlayer Wins!\n";
+                    cout << "You won: " << pot << "$\n";
+                    playerMoney += pot;
+                }
+                else {
+                    cout << "House Wins!\n";
+                    cout << "Better luck next time\n";
+                    houseMoney += pot;
+                }
+                _getch();
+                break; // AI End-----------------------------------------------------------------------------------------------------------------------------------
 
             default:
                 break;
             }
         } while (!validInput);
+
+        system("cls");
     } while (!hold);
 }
 
-void drawCard(int offset, int chosenCard) {
+void drawCard(int offset, int chosenCard, int player, bool bank) {
     string cardTop{ ".--------." };
     string cardBottom{ "'--------'" };
-    string fillTop{ ".--.  |" };
-    string fillBottom{ "|  '--'" };
-    vector <int> playerCards(10);
-    vector <int> houseCards(10);
     ostringstream line1;
     ostringstream line2;
     ostringstream line3;
@@ -146,8 +224,8 @@ void drawCard(int offset, int chosenCard) {
 
     deck[chosenCard].drawn = true;
 
-    line1 << "|" << deck[chosenCard].name << deck[chosenCard].suit << fillTop;
-    line4 << fillBottom << deck[chosenCard].name << deck[chosenCard].suit << "|";
+    line1 << "|" << deck[chosenCard].name << deck[chosenCard].suit << ".--.  |";
+    line4 << "|  '--'" << deck[chosenCard].name << deck[chosenCard].suit << "|";
 
     if (deck[chosenCard].suit == hearts) {
         line2 << "|  (\\/)  |";
@@ -166,18 +244,35 @@ void drawCard(int offset, int chosenCard) {
         line3 << "|  (__)  |";
     }
 
-    for (int j{ 0 }; j < 10; ++j) {
-        cardPrint[0][offset + j] = cardTop[j];
-        cardPrint[1][offset + j] = line1.str()[j];
-        cardPrint[2][offset + j] = line2.str()[j];
-        cardPrint[3][offset + j] = line3.str()[j];
-        cardPrint[4][offset + j] = line4.str()[j];
-        cardPrint[5][offset + j] = cardBottom[j];
+    if (bank == false) {
+        for (int j{ 0 }; j < 10; ++j) {
+            cardPrint[0][offset + j] = cardTop[j];
+            cardPrint[1][offset + j] = line1.str()[j];
+            cardPrint[2][offset + j] = line2.str()[j];
+            cardPrint[3][offset + j] = line3.str()[j];
+            cardPrint[4][offset + j] = line4.str()[j];
+            cardPrint[5][offset + j] = cardBottom[j];
+        }
+    }
+    else {
+        for (int j{ 0 }; j < 10; ++j) {
+            aiCardPrint[0][offset + j] = cardTop[j];
+            aiCardPrint[1][offset + j] = line1.str()[j];
+            aiCardPrint[2][offset + j] = line2.str()[j];
+            aiCardPrint[3][offset + j] = line3.str()[j];
+            aiCardPrint[4][offset + j] = line4.str()[j];
+            aiCardPrint[5][offset + j] = cardBottom[j];
+        }
     }
 
     for (int i{ 0 }; i < 6; ++i) {
         for (int j{ 0 }; j < 100; ++j) {
-            cout << cardPrint[i][j];
+            if (player == 1) {
+                cout << cardPrint[i][j];
+            }
+            else {
+                cout << aiCardPrint[i][j];
+            }
         }
         cout << '\n';
     }
